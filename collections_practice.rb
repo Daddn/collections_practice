1e4f535f1d5c619fceeec9d203fd3f569e4c6701
andrewbay <daddnbay@gmail.com>
2021-01-18 13:06:48 -0800

    commit tree bd20dfce051fef12b284aa48124e52d41da2bb2d

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort { |a, b| [a.size, a] <=> [b.size, b] }
+end
+
+def swap_elements(array)
+    array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each { |character| character [2] = '$' }
+end
+
+def find_a(array)
+    array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array.collect do |word| 
+        if array[1] == word
+            word
+        else
+            word + 's'
+        end
+    end
+end
